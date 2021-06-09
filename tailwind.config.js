80e65be2e092e74799d8ef84d2f9667e419c8a4c
Tatiana <Tatiszulik777@gmail.com>
2021-06-09 16:35:03 +0200

    changed directories

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: ['./tailwindprojects/**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        "back": "#203674",
+        "left": '#071a50',
+        "right": '#f1f5f8',
+        "bor": "#2d427b",
+        "fontText": "#b5c8ff",
+        "before":"#2b4286",
+      },
+      width:{
+        "bac": "95%",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
