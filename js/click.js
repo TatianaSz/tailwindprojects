9ac5baac21de7cfe5590f0b92fa92e508ea51cfd
Tatiana <Tatiszulik777@gmail.com>
2021-06-13 14:42:09 +0200

    switching icons

--- js/click.js
+++ js/click.js
@@ -1,6 +1,25 @@
 const burger = document.getElementById("burger");
+
 const afterClick = function(e){
 let tog = document.getElementById("toToggle");
+const iconBurger = document.querySelector(".fafaIcon");
 tog.classList.toggle("hidden");
+let x = iconBurger.classList.contains("fa-bars")
+switch(x){
+    case true:
+    iconBurger.classList.remove("fa-bars")
+    iconBurger.classList.add("fa-times")
+    break;
+    case false:
+    iconBurger.classList.remove("fa-times")
+    iconBurger.classList.add("fa-bars")    
+    break;
+}
+
 }
 burger.addEventListener("click", afterClick)
+
+
+
+
+//fas fa-bars burger fas fa-times - x 
